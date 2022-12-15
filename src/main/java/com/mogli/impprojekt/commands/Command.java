d8f3a54f94825d04dcb9c4fe53f8243c9f1a4e7d
Moritz Becker <50841028+Moglis07@users.noreply.github.com>
2022-12-15 15:23:21 +0100

    Delete src/main/java/com/mogli/impprojekt directory

--- src/main/java/com/mogli/impprojekt/commands/Command.java
+++
@@ -1,9 +0,0 @@
-package com.mogli.impprojekt.commands;
-
-import java.io.IOException;
-
-public interface Command {
-
-    public void onCommand(String[] args) throws IOException;
-
-}
--- src/main/java/com/mogli/impprojekt/commands/ExitCMD.java
+++
@@ -1,9 +0,0 @@
-package com.mogli.impprojekt.commands;
-
-public class ExitCMD implements Command {
-    @Override
-    public void onCommand(String[] args) {
-        System.out.println("Save and Exit");
-        System.exit(0);
-    }
-}
